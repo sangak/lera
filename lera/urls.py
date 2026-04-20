e6c40b5cbf9fb9527b69038656599f6473858b99
suryaindustri <mawardi_mhd@yahoo.co.id>
2026-04-20 10:15:12 +0700

    modified CSRF config

--- lera/urls.py
+++ lera/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from django.views.generic import RedirectView
 
 urlpatterns = [
@@ -26,3 +28,6 @@ urlpatterns = [
     path('dashboard/', include('dashboard.urls', namespace='dashboard')),
     path('i18n/', include('django.conf.urls.i18n')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
