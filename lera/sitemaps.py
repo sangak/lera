f45946afaa5ec5d275718046ed50171ead045566
suryaindustri <mawardi_mhd@yahoo.co.id>
2026-04-22 22:24:53 +0700

    add sitemap.xml

---
+++ lera/sitemaps.py
@@ -0,0 +1,26 @@
+import pendulum
+from datetime import timezone
+from django.contrib.sitemaps import Sitemap
+from django.shortcuts import reverse
+
+
+class StaticSitemap(Sitemap):
+    priority = 0.5
+    # changefreq = 'weekly'
+    protocol = 'https'
+
+    def items(self):
+        return [
+            'landing:index',
+            'landing:about',
+            'landing:vision-mission',
+            'landing:program',
+            'landing:contact'
+        ]
+
+    def location(self, item):
+        return reverse(item)
+
+    def lastmod(self, item):
+        now = pendulum.now('Asia/Jakarta')
+        return now
--- lera/urls.py
+++ lera/urls.py
@@ -18,7 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.sitemaps.views import sitemap
 from django.views.generic import RedirectView
+from .sitemaps import StaticSitemap
+
+sitemaps = {
+    'static': StaticSitemap
+}
 
 urlpatterns = [
     path('', include('landing.urls', namespace='landing')),
@@ -27,6 +33,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('dashboard/', include('dashboard.urls', namespace='dashboard')),
     path('i18n/', include('django.conf.urls.i18n')),
+    path('sitemap.xml/', sitemap, {'sitemaps': sitemaps}, name='sitemap'),
 ]
 if '127.0.0.1:8000' in settings.BASE_URL:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
