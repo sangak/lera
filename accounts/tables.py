1a177c70d0318986a6bae900bbefaa3e18df773b
suryaindustri <mawardi_mhd@yahoo.co.id>
2026-04-23 19:21:02 +0700

    modified tables buttons and meta

--- accounts/tables.py
+++ accounts/tables.py
@@ -37,25 +37,43 @@ class AccountListTable(BaseTables):
         "th": {"style": "width: 80px"},
         "td": {"class": "text-center"},
     })
+    action = tables.Column(empty_values=(), verbose_name="")
 
     def __init__(self, *args, **kwargs):
         super(AccountListTable, self).__init__(*args, **kwargs)
         self.counter = itertools.count(1)
 
     class Meta(BaseTables.Meta):
-        attrs = {
-            'tbody': {
-                'hx_post': 'cek'
-            }
-        }
+        pass
+        # meta = BaseTables.Meta
+        # atr = {
+        #     'class': 'tes'
+        # }
+        # attrs = {
+        #     'class': f"{meta.attrs.get('class')} {atr['class']}",
+        #     'style': "min-height: 300px"
+        # }
+
 
     def render_row_number(self):
         return next(self.counter)
 
     def render_active(self, value):
-        badge = '<iconify-icon icon="mingcute:check-fill" width="16px" height="16px" class="text-success"></iconify-icon>' if value == True else '<iconify-icon icon="maki:cross" width="16px" height="16px" class="text-danger"></iconify-icon>'
+        if value:
+            badge = '<iconify-icon icon="mingcute:check-fill" width="16px" height="16px" class="text-success"></iconify-icon>'
+        else:
+            badge = '<iconify-icon icon="maki:cross" width="16px" height="16px" class="text-danger"></iconify-icon>'
         return format_html(badge)
 
     def render_status(self, value):
         status = "Admin" if value else ""
         return status
+
+    def render_action(self, value):
+        btn = '''
+           <div class="btn-group" role="group" aria-label="Basic example">
+              <button type="button" class="btn btn-sm btn-secondary">Left</button>
+              <button type="button" class="btn btn-sm btn-secondary">Right</button>
+            </div>
+        '''
+        return format_html(btn)
