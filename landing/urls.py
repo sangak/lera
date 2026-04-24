3bd70b4d3489c28019cf5a9dacc69a54c6e423e0
suryaindustri <mawardi_mhd@yahoo.co.id>
2026-04-24 16:18:12 +0700

    add site.webmanifest

--- landing/urls.py
+++ landing/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from landing.views import (
     LandingPageView, ContactUsPageView,
-    ProgramPageView, AboutUsPageView, VisionPageView
+    ProgramPageView, AboutUsPageView, VisionPageView, WebManifestPageView
 )
 
 app_name = 'landing'
@@ -12,5 +12,6 @@ urlpatterns = [
     path('contact-us/', ContactUsPageView.as_view(), name='contact'),
     path('program/', ProgramPageView.as_view(), name='program'),
     path('vision-mission/', VisionPageView.as_view(), name='vision-mission'),
+    path('site.webmanifest/', WebManifestPageView.as_view(), name='webmanifest'),
 ]
 
